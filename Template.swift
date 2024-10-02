d4674be79841dfe4d4936f55d571da3ab306f2aa
fortmarek <marekfort@me.com>
2024-10-02 08:55:15 +0200

    Add Package.swift to template

--- Template.swift
+++ Template.swift
@@ -9,9 +9,8 @@ var items: [Template.Item] = [
     //Project Template
     .file(path: projectPath + "/Project.swift", templatePath: "Project.stencil"),
     .file(path: appPath + "\(nameAttribute).swift", templatePath: "Main.stencil"),
-
+    .file(path: projectPath + "/Tuist/Package.swift", templatePath: "Package.stencil"),
 ]
-
 
 let template = Template(
     description: "Custom template",
